70059c96eb59181a5bba1703899d43674b817492
Sefa Sia Deniz <sefasaiddeniz1@gmail.com>
2017-03-03 04:46:41 +0300

    Created date range picker

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,27 @@
 /**
  * Created by sefasaid on 03/03/2017.
  */
-angular.module('getir-frontend', ['getir-services'])
-    .controller('MyController', function(getRecords) {
+angular.module('getir-frontend', ['getir-services','daterangepicker'])
+    .controller('MyController', function($scope,getRecords) {
 
+        $scope.date = {
+            startDate: moment().subtract(1, "days"),
+            endDate: moment()
+        };
+        $scope.options = {
+            applyClass: 'btn-green',
+            locale: {
+                applyLabel: "Onayla",
+
+                format: "D-MMMM-YY", //will give you 6-January-17
+
+                cancelLabel: 'İptal',
+                customRangeLabel: 'Özel Alan'
+            },
+            ranges: {
+                'Son 7 Gün': [moment().subtract(6, 'days'), moment()],
+                'Son 30 Gün': [moment().subtract(29, 'days'), moment()]
+            }
+
+        }
     });
