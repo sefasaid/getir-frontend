f5b17d8335e65e28f4391413545ad191a09688e8
Sefa Sia Deniz <sefasaiddeniz1@gmail.com>
2017-03-04 01:42:48 +0300

    Added log for find port

--- web.js
+++ web.js
@@ -6,4 +6,6 @@ var express = require('express');
 var app = express();
 
 app.use(gzippo.staticGzip("" + __dirname + "/"));
-app.listen(process.env.PORT || 5000);
+var port =  process.env.PORT || 5000
+app.listen(port);
+console.log("App is running in "+ port);
